89c10472d87ae6660821efe6200246197b3faa54
iyidaclem <iyidaclem@gmail.com>
2023-05-13 00:26:57 +0100

    ERC1155 uri id substitution implemented

--- contracts/BNUGEvent.sol
+++ contracts/BNUGEvent.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/extensions/ERC1155SupplyUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract BNUGEvent is
     Initializable,
@@ -22,16 +23,15 @@ contract BNUGEvent is
     uint256 public devs_fee;
     string public name;
     string public symbol;
+    string private _uri;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
-    function initialize() public initializer {
-        __ERC1155_init(
-            "https://ipfs.io/ipfs/QmPhCd92UR2EjeVeoHxkJRnf9g3UBkyPeoU2kfX9R1ApnS"
-        );
+    function initialize(string calldata uri_) public initializer {
+        __ERC1155_init(uri_);
         __Ownable_init();
         __Pausable_init();
         __ERC1155Supply_init();
@@ -40,10 +40,19 @@ contract BNUGEvent is
         devs_fee = 30 ether;
         name = "BNUGDAO EVENT";
         symbol = "BNUGDAO";
+        _uri = uri_;
     }
 
     function setURI(string memory newuri) public onlyOwner {
+        _uri = newuri;
         _setURI(newuri);
+    }
+
+    function uri(uint256 tokenId) public view override returns (string memory) {
+        return
+            string(
+                abi.encodePacked(_uri, "/", Strings.toString(tokenId), ".json")
+            );
     }
 
     function setMintingFee(uint8 id, uint256 new_fee) public onlyOwner {
--- scripts/event.js
+++ scripts/event.js
@@ -2,11 +2,10 @@ const { ethers, upgrades } = require('hardhat');
 const fs = require('fs');
 async function main() {
   const BNUGEvent = await ethers.getContractFactory('BNUGEvent');
-  const bNUGEvent = await upgrades.deployProxy(BNUGEvent, [], {
+  const bNUGEvent = await upgrades.deployProxy(BNUGEvent, ["https://ipfs.io/ipfs/QmS4eVEtY1Av8hWnPmuB4tyRZMKvGoffTg8BEZgLLxCT8y/event_metadata"], {
     initializer: 'initialize',
   });
   await bNUGEvent.deployed();
-
   console.log('Event deployed to:', bNUGEvent.address);
   fs.writeFileSync(process.env.MODE === 'prod' ? 'deployed_event_address' : 'dev_deployed_event_address', `${bNUGEvent.address}`, { encoding: 'utf-8' });
 
