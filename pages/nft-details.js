3f271540d1be635e8e38b0e4d46b0acea9874380
iyidaclem <iyidaclem@gmail.com>
2023-05-17 13:58:46 +0100

    Check it out page fixed

--- pages/nft-details.js
+++ pages/nft-details.js
@@ -190,7 +190,7 @@ const AssetDetails = () => {
                 btnName="Check it out"
                 btnType="primary"
                 classStyles="sm:mr-0 sm:mb-5 rounded-xl"
-                handleClick={() => router.push('/my-nfts')}
+                handleClick={() => router.push('/profile')}
               />
             </div>
           )}
